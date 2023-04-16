456f8843c62f8f800b83c5fa447003baa8f51d7f
Andrew Martin <10902965+AndrewR3K@users.noreply.github.com>
2023-04-16 13:43:28 -0700

    docs fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     target: 'static',
-    router: { options: { strict: true, }, },
     modules: [
         '@nuxtjs/tailwindcss',
         'nuxt-gtag',
