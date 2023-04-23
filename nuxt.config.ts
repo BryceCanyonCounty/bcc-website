2119220d92e03797506cc994b52efd8b9a5939c4
AndrewR3K <10902965+AndrewR3K@users.noreply.github.com>
2023-04-22 22:52:48 -0700

    doc code highlight fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    target: 'static',
-    modules: [
-        '@nuxtjs/tailwindcss',
-        'nuxt-gtag',
-        '@nuxt/content'
-    ],
-    
-    content: {
-        // https://content.nuxtjs.org/api/configuration
-        
-    },    
-    
-    gtag: {
-        id: 'G-D2F2LGNEDM'
-    }
-})
+  target: "static",
+  modules: ["@nuxtjs/tailwindcss", "nuxt-gtag", "@nuxt/content"],
+
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+    highlight: {
+      // Theme used in all color schemes.
+      theme: "github-dark",
+      preload: ["js", "json", "javascript", "lua"],
+    },
+  },
+
+  gtag: {
+    id: "G-D2F2LGNEDM",
+  },
+});
